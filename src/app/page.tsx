184c346eeea92bbc718dee3cda407dab6c4f7c3e
devCjay <edehchijioke09@gmail.com>
2025-11-11 00:47:05 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 const Page = () => {
   return (
     <div className="text-red-500 text-center">
     <h1 className="text-6xl font-bold">
           Hello world! This is Next.js
+          <br />
+          <Button className="ml-4">Click Me</Button>
     </h1>
     </div>
   );

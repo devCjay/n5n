964efabff69d2f436187537a408ac412dc4821be
devCjay <edehchijioke09@gmail.com>
2025-11-11 14:03:18 +0100

    02: database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import { Button } from "@/components/ui/button";
+import prisma from "@/lib/db";
 
-const Page = () => {
+
+const Page = async () => {
+   const users = await prisma.user.findMany();
   return (
     <div className="text-red-500 text-center">
     <h1 className="text-6xl font-bold">
           Hello world! This is Next.js
           <br />
           <Button className="ml-4">Click Me</Button>
+          <br />
+          {JSON.stringify(users)}
     </h1>
     </div>
   );
